4ef32a6316ed7f75ddbb70681ff5f92618e1ab93
Gianpaolo Raimondi <gianpaolo.raimondi@gmail.com>
2020-12-15 21:40:40 +0100

    added navbar, herosection and background

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,7 +1,16 @@
 import React, { useState, useEffect } from "react"
 import { FaBars, FaTimes } from "react-icons/fa"
 import { IconContext } from "react-icons/lib"
-import {Nav, NavbarContainer, NavLogo, NavIcon, MobileIcon} from "./NavbarElements"
+import {
+  Nav,
+  NavbarContainer,
+  NavLogo,
+  NavIcon,
+  MobileIcon,
+  NavMenu,
+  NavItem,
+  NavLinks,
+} from "./NavbarElements"
 
 const Navbar = () => {
   const [click, setClick] = useState(false)
@@ -32,8 +41,19 @@ const Navbar = () => {
               EXPLORE
             </NavLogo>
             <MobileIcon onClick={handleClick}>
-                {click ? <FaTimes /> : <FaBars />}
+              {click ? <FaTimes /> : <FaBars />}
             </MobileIcon>
+            <NavMenu onClick={handleClick} click={click}>
+              <NavItem>
+                <NavLinks to="/">Home</NavLinks>
+              </NavItem>
+              <NavItem>
+                <NavLinks to="/images">Images</NavLinks>
+              </NavItem>
+              <NavItem>
+                <NavLinks to="/destinations">Destinations</NavLinks>
+              </NavItem>
+            </NavMenu>
           </NavbarContainer>
         </Nav>
       </IconContext.Provider>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import HeroSection from "../components/HeroSection"
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -5,9 +6,8 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <HeroSection />
+    <h1>Content</h1>
   </Layout>
 )
 
