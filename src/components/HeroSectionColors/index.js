648713d74d35c1ca1e1c333fd16b83d31e67fbb7
Gianpaolo Raimondi <gianpaolo.raimondi@gmail.com>
2020-12-18 23:39:34 +0100

    added colors and revised site structure

---
+++ src/components/HeroSectionColors/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+import "./HeroSectionColors.css"
+import { Button } from "../ButtonElements"
+
+function HeroSectionColors() {
+  return (
+    <div className="hero-container-color">
+      <h1>colore</h1>
+    </div>
+  )
+}
+
+export default HeroSectionColors
---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const About = () => (
+  <Layout>
+    <SEO title="about" />
+    <h1 className="about">About</h1>
+  </Layout>
+)
+
+export default About
